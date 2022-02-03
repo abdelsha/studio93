39bc2f171c4bdb73dd5ab2c140aebb80b1bb5892
abdelsha <49315774+abdelsha@users.noreply.github.com>
2022-02-02 20:17:07 -0500

    edit1

--- src/Pages/Gallery/Gallery.js
+++ src/Pages/Gallery/Gallery.js
@@ -1,7 +1,57 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { useEffect } from "react";
+import { useRef } from "react";
+import LocomotiveScroll from "locomotive-scroll";
+import "./Gallery.css"
 function Gallery() {
-  return <div></div>;
+
+  const ref = useRef(null);
+const[imagedata,setImageData]=useState([])
+
+
+const images =()=>{
+   for(let i=0; i<19;i++){
+      setImageData((prev)=>[...prev,"/images/"+`Richmond${i}`+".jpg"])
+      //console.log(imagedata)
+   }
+ }
+
+ useEffect(()=>{
+   images();
+   //console.log(imagedata)
+ },[])
+  
+  return (
+    <div className='Gallary_main'>
+      {/* {console.log(imagedata)} */}
+      {imagedata.map((image,index)=>(
+        <> 
+        <div className='Gallary_images'>
+          <div className='Gallary_images_left'>
+            {/* <img src={image} alt=""></img> */}
+          </div>
+          <div className='Gallary_images_right'>
+            <div className='Gallary_images_righttop'>
+              {/* <img src={image} alt=""></img> */}
+            </div>
+            <div className='Gallary_images_rightbottom'>
+              {/* <img src={image} alt=""></img> */}
+            </div>
+          </div>
+          
+        </div>
+        
+        <div className='Gallary_images'>
+          {/* <img src={image} alt=""></img> */}
+        </div>
+        {/* <img src={image} alt=""></img> */}
+        </>
+        
+        
+      ))}
+      
+    </div>
+  );
 }
 
 export default Gallery;
--- src/Redux/Actions/UserState.js
+++ src/Redux/Actions/UserState.js
@@ -3,8 +3,87 @@ import db from "../../Firebase/Firebase";
 
 //Action:
 const email = "amrshakour97@gmail.com";
+export const SET_USER = "SET_USER";
+export const SET_LOADING_STATUS = "SET_LOADING_STATUS";
+export const GET_ARTICLES = "GET_ARTICLES";
+export const GET_EXPERIENCE= "GET_EXPERIENCE";
+export const GET_PROFILE="GET_PROFILE";
 
 export const setUser = (user) => ({
-  type: "SET_USER",
+  type: SET_USER,
   payload: user,
 });
+
+export const getArticles = (payload) => ({
+  type: GET_ARTICLES,
+  payload: payload,
+});
+
+export function postArticleApi(payload) {
+  return (dispatch) => {
+      dispatch(setLoading(true));
+
+
+      if (payload.image !=""){
+          const upload = storage
+          .ref(`images/${payload.image.name}`)
+          .put(payload.image);
+      upload.on('state_changed', (snapshot) => {
+          const progress =
+              (snapshot.bytesTransferred/snapshot.totalBytes) *100;
+
+          //console.log(`progress: ${progress}%`);
+          if (snapshot.state === "RUNNING") {
+              console.log(`progress: ${progress}%`);
+          }
+      }, error => console.log(error.code),
+      async () => {
+          const downloadURL = await upload.snapshot.ref.getDownloadURL();
+          db.collection(`article`).add({
+              actor: {
+                  description: payload.user.email,
+                  title: payload.user.displayName,
+                  date: payload.timestamp, 
+                  image: payload.user.photoURL
+              },
+              video: payload.video,
+              sharedImg: downloadURL,
+              comments:0,
+              description:payload.description,
+          })
+          dispatch(setLoading(false));
+      }
+      )
+  }else if (payload.video){
+      db.collection(`article`).add({
+          actor: {
+              description: payload.user.email,
+              title: payload.user.displayName,
+              date: payload.timestamp, 
+              image: payload.user.photoURL
+          },
+          video: payload.video,
+          sharedImg: "",
+          comments:0,
+          description:payload.description,
+      })
+      dispatch(setLoading(false));
+  }
+      
+  };
+}
+
+export function getArticlesAPI() {
+  return (dispatch) => {
+      let payload;
+      
+      db.collection("article")
+      .orderBy("actor.date","desc")
+      .onSnapshot((snapshot) => {
+          payload= snapshot.docs.map((doc) => doc.data());
+          //console.log(payload)
+          dispatch(getArticles(payload));
+      });
+  };
+}
+
