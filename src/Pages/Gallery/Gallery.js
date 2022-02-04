aa38d35d5da1c1f2f778b676eba0085b734213bb
abdelsha <49315774+abdelsha@users.noreply.github.com>
2022-02-03 21:59:29 -0500

    add horizonatal scrolling

--- src/Pages/Gallery/Gallery.js
+++ src/Pages/Gallery/Gallery.js
@@ -2,7 +2,11 @@ import React, { useState } from 'react';
 import { useEffect } from "react";
 import { useRef } from "react";
 import LocomotiveScroll from "locomotive-scroll";
-import "./Gallery.css"
+import "./Gallery.css";
+import HorizontalScroll from 'react-scroll-horizontal';
+
+
+
 function Gallery() {
 
   const ref = useRef(null);
@@ -20,62 +24,98 @@ const images =()=>{
    }
   
  }
+// get the window size and check the width, if its smaller than 768 then 
+//know its mobile and you do not want to change the scrolling direction
+
+const onWheel = e => {
+  e.preventDefault();
+  const container = scrollRef.current;
+  const containerScrollPosition = scrollRef.current.scrollLeft;
+
+  container.scrollTo({
+    top: 0,
+    left: containerScrollPosition + e.deltaY + e.deltaX,
+  });
+};
+
+const scrollRef = useRef(null);
 
  useEffect(()=>{
    images();
    console.log(imagedata);
+  var element = document.scrollingElement || document.documentElement;
+  element.addEventListener('wheel', onWheel, { passive: false });
  },[])
 
  
   
   return (
-    <div className='Gallary_main'>
-      
-      
-      
-      
-      {/* {console.log(imagedata)} */}
-      {imagedata.map((imagegroup,index)=>{
-        let key=index
-        return (
-          <>
-          <img className='Gallery_images' src={imagegroup[0]} alt=""></img>
-          <img className='Gallery_images' src={imagegroup[1]} alt=""></img>
-          <img className='Gallery_images' src={imagegroup[2]} alt=""></img>
-          <img className='Gallery_images' src={imagegroup[3]} alt=""></img>
-          </>
-        )
-      })}
-      {/* {imagedata.map((image,index)=>(
-        
-        <> 
-        
-          <div className='Gallary_images_left'>
-            <img className='Gallery_images' src={image} alt=""></img>
-          </div>
-          <div className='Gallary_images_right'>
-            <div className='Gallary_images_righttop'>
-              <img className='Gallery_images' src={image} alt=""></img>
-            </div>
-            <div className='Gallary_images_rightbottom'>
-              <img className='Gallery_images' src={image} alt=""></img>
-            </div>
-          </div>
-          
-        
-        
-        <div className='Gallary_images'>
-          <img className='Gallery_images' src={image} alt=""></img>
-        </div>
-        
-        </>
-        
-        
-      ))} */}
-      
-      
-    </div>
+    
+      <div className="Gallary_main container" ref={scrollRef}  id="container">
+        {/* {console.log(imagedata)} */}
+        {console.log(window.pageXOffset)}
+        {imagedata.map((imagegroup, index) => {
+          let key = index;
+          return (
+            <>
+              <div className="Gallery_images1">
+                <img src={imagegroup[0]} alt=""></img>
+              </div>
+              <div className="Gallery_right">
+                <img
+                  className="Gallery_images2"
+                  src={imagegroup[1]}
+                  alt=""
+                ></img>
+                <img
+                  className="Gallery_images3"
+                  src={imagegroup[2]}
+                  alt=""
+                ></img>
+              </div>
+              <div className="Gallery_images4">
+                <img src={imagegroup[3]} alt=""></img>
+              </div>
+            </>
+          );
+        })}
+      </div>
+    
   );
 }
 
 export default Gallery;
+
+
+/*<HorizontalScroll>
+      <div className="Gallary_main ">
+        
+        {console.log(window.pageYOffset)}
+        {imagedata.map((imagegroup, index) => {
+          let key = index;
+          return (
+            <>
+              <div className="Gallery_images1">
+                <img src={imagegroup[0]} alt=""></img>
+              </div>
+              <div className="Gallery_right">
+                <img
+                  className="Gallery_images2"
+                  src={imagegroup[1]}
+                  alt=""
+                ></img>
+                <img
+                  className="Gallery_images3"
+                  src={imagegroup[2]}
+                  alt=""
+                ></img>
+              </div>
+              <div className="Gallery_images4">
+                <img src={imagegroup[3]} alt=""></img>
+              </div>
+            </>
+          );
+        })}
+      </div>
+    </HorizontalScroll> 
+    */
