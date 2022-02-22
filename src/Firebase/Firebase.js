4453a48ba73ab6ef7a94d92865e8991865d732c5
Amr Shakour <amrshakour97@gmail.com>
2022-02-22 16:53:32 -0500

    updated gallery and linkedin link

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -2,14 +2,14 @@ import firebase from "firebase";
 import React from "react";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBgEdk-_yAo-J3c2MuFjvdUzC4ZrVazk00",
-    authDomain: "studio93-fae8c.firebaseapp.com",
-    projectId: "studio93-fae8c",
-    storageBucket: "studio93-fae8c.appspot.com",
-    messagingSenderId: "1008713688567",
-    appId: "1:1008713688567:web:fa6a232a8727c7ca7d3af2",
-    measurementId: "G-5PBZQFXCH7"
-  };
+  apiKey: "AIzaSyDYSU2RgphX7IdL0UGcolBVCdf_ogm8y0M",
+  authDomain: "studio93inc-c63ac.firebaseapp.com",
+  projectId: "studio93inc-c63ac",
+  storageBucket: "studio93inc-c63ac.appspot.com",
+  messagingSenderId: "866810378859",
+  appId: "1:866810378859:web:8a5b6ac500e050142acf2e",
+  measurementId: "G-QKKN2049XV"
+};
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
